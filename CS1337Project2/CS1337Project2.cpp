2dc70d71ba617df016cb1cf52336498c293eaf8e
JamesRobRob <Jamespr.0312@gmail.com>
2016-10-05 13:05:41 -0500

    Just trying stuff

--- CS1337Project2/CS1337Project2.cpp
+++ CS1337Project2/CS1337Project2.cpp
@@ -1,11 +1,31 @@
-// CS1337Project2.cpp : Defines the entry point for the console application.
-//
+#include <iostream>
+#include <fstream>
 
-#include "stdafx.h"
+using namespace std;
 
+int main(){
+	char* answerStart;
+	ifstream inFile;
+	
+	inFile.open("answers.txt", ios::in);
 
-int main()
-{
+	//Failsafe, if we can't read the file don't even run the program
+	if (inFile) {
+		cout << "\nFilestream read error\n" << endl;
+
+		system("pause");
+		return 0;
+	}
+	
+	//assuming the file goes: a b c d e f. We can use ifstream.ignore to skip whitespace
+	for (int i = 0; !inFile.eofbit; i++) {
+		*(answerStart + i) = inFile.get();
+		inFile.ignore();
+	}
+
+	for (int i = 0; i < 10; i++) {
+		cout << *(answerStart + i) << endl;
+	}
+
     return 0;
 }
-
