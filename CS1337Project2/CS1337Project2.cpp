9e3556c29ca52181a01924ed4c8de47eaba42f95
JamesRobRob <Jamespr.0312@gmail.com>
2016-10-10 13:50:08 -0500

    Please god help me

--- CS1337Project2/CS1337Project2.cpp
+++ CS1337Project2/CS1337Project2.cpp
@@ -6,8 +6,10 @@ using namespace std;
 int main(){
 	char* answerStart;
 	ifstream inFile;
+
+	answerStart = new char;
 	
-	inFile.open("answers.txt", ios::in);
+	inFile.open("../answers.txt", ios::in);
 
 	//Failsafe, if we can't read the file don't even run the program
 	if (inFile) {
@@ -16,16 +18,30 @@ int main(){
 		system("pause");
 		return 0;
 	}
+
+	for (int i = 0; i < 5; i++) {
+		cin >> *(answerStart + i);
+		cout << "Out: " << *(answerStart + i) << endl;
+	}
+
+	for (int i = 0; i < 5; i++) {
+		cout << *(answerStart + i) << " ";
+	}
 	
 	//assuming the file goes: a b c d e f. We can use ifstream.ignore to skip whitespace
+	/*
 	for (int i = 0; !inFile.eofbit; i++) {
-		*(answerStart + i) = inFile.get();
+		*(answerStart += i) = inFile.get();
 		inFile.ignore();
-	}
-
-	for (int i = 0; i < 10; i++) {
 		cout << *(answerStart + i) << endl;
 	}
+	
 
+	for (int i = 0; i < 10; i++) {
+		cout << endl << *(answerStart + i);
+	}
+	*/
+	system("pause");
     return 0;
 }
+	
---
+++ CS1337Project2/PointerLinkedList.cpp
@@ -0,0 +1,31 @@
+/*
+template<class t>
+class PointerLinkedList {
+	 node head;
+
+public:
+	t getAt() {
+	}
+	t setAt() {
+
+	}
+	void add(t) {
+
+	}
+	t removeAt() {
+
+	}
+	int size() {
+
+	}
+};
+
+template<class t>
+class node<t> {
+	node<t> next;
+	t data;
+
+	void setData(t)
+};
+
+*/
